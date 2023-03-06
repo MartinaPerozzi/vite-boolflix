fbad07f7d123c43bc0b1e9fde2862e0cdc6e796f
MartinaPerozzi <perozzimartina@gmail.com>
2023-03-06 19:16:19 +0100

    -still not working

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -18,7 +18,7 @@ export default {
     <div class="card-container">
         <AppCard v-for="movie in store.movies" :key="movie.id" :title="movie.title" :originalTitle="movie.original_title"
             :lang="movie.original_language" :vote="movie.vote_average" :pic="movie.poster_path" class="me-2 mb-2"
-            :overview="movie.overview" />
+            :overview="movie.overview" :id="movie.id" />
         <AppCard v-for="serie in store.series" :key="serie.id" :title="serie.title" :originalTitle="serie.original_title"
             :lang="serie.original_language" :vote="serie.vote_average" :pic="serie.poster_path" class="me-2 mb-2" />
 
